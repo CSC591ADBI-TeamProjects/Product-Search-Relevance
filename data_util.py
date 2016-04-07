7863fb3ce8474063fd1dc1ad6ce670d5d25900be
arjundatt <arjundatt.16@gmail.com>
2016-04-07 12:40:20 -0400

    Stemming of words in training data

---
+++ data_util.py
@@ -0,0 +1,29 @@
+import sys
+import pandas as pd
+from nltk.stem.snowball import SnowballStemmer
+
+path_to_data = "data/"
+stemmer = SnowballStemmer("english")
+
+def join_data():
+  df_train = pd.read_csv(path_to_data+'train.csv', encoding="ISO-8859-1")
+  #stemming of training data
+  df_train["search_term"] = df_train['search_term'].map(lambda x:str_stemmer(x))
+  df_train['product_title'] = df_train['product_title'].map(lambda x:str_stemmer(x))
+  
+  df_test = pd.read_csv(path_to_data+'test.csv', encoding="ISO-8859-1")
+  
+  df_pro_desc = pd.read_csv(path_to_data+'product_descriptions.csv', encoding="ISO-8859-1")
+  #stemming of product_description data - takes ENORMOUS TIME, remove if not needed
+  df_pro_desc['product_description'] = df_pro_desc['product_description'].map(lambda x:str_stemmer(x))
+  
+  #TODO do stemming for attributes data also
+  df_attr = pd.read_csv(path_to_data+'attributes.csv', encoding="ISO-8859-1")
+  
+  df_desc_attr = pd.merge(df_pro_desc, df_attr, how='left', on='product_uid')
+  df_all = pd.merge(df_train, df_desc_attr, how='left', on='product_uid')
+  return df_all,df_test
+
+#takes a string and returns a string with words replaced with their stems
+def str_stemmer(s):
+	return " ".join([stemmer.stem(word) for word in s.lower().split()])
---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+import data_util as dutil
+import pandas as pd
+
+
+def main():
+  df_all_train,df_test = dutil.join_data()
+
+
+if __name__=="__main__":
+  main()
