a8358acace435e69352d2f95d2aa6055da9217bc
Rajat Shah <rajat.shah200792@gmail.com>
2016-04-08 17:26:53 -0400

    adding a sample feature

--- feature_util.py
+++ feature_util.py
@@ -3,6 +3,18 @@ Contains methods to extract features for training
 '''
 import pandas as pd
 
-#get the feature vector from the training data
+'''
+Feature X vector (a column) to be added to final features data frame
+'''
+def get_feature_x(train):
+  feature = []
+  #ensure the size is as required
+  return feature
+
+'''
+Returns features from all the feature extraction methods
+'''
 def get_features(train):
-  return
+  features = pd.pd.DataFrame(columns = colNames)
+  feature['X'] = get_feature_x(train)
+  return features
