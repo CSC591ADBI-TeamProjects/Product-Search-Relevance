d90161bbddb6527058935c52e5da320b276ae38d
Rajat Shah <rajat.shah200792@gmail.com>
2016-04-08 17:38:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ model_param = sys.argv[2]
 path_to_data = sys.argv[1]
 
 def main():
-#  df_all_train,df_test = dutil.join_data(path_to_data)
+  df_all_train,df_test = dutil.join_data(path_to_data)
   features = feature_util.get_features(df_all_train)
   result = getattr(model_util, model_param)(df_all_train,df_test)
 
